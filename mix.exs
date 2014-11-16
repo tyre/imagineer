d9f4e2e56d871ecf67aab63e28d3df32ad9f332f
Chris Maddox <tyre77@gmail.com>
2014-11-16 10:28:27 -0800

    add description;

--- mix.exs
+++ mix.exs
@@ -2,28 +2,19 @@ defmodule Imagineer.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :imagineer,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     deps: deps]
+    [
+      app: :imagineer,
+      version: "0.1.0",
+      elixir: "~> 1.0",
+      deps: deps,
+      description: "Image processing in Elixir"
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
   end
