bbaa751fbd5abd94933d0f36ba4ccb01a761ebb3
Chris Maddox <tyre77@gmail.com>
2015-06-25 23:12:05 -0700

    add better error messages

--- lib/imagineer.ex
+++ lib/imagineer.ex
@@ -2,11 +2,17 @@ defmodule Imagineer do
   alias Imagineer.FormatDetector
   alias Imagineer.Image.PNG
 
+  @doc """
+  Loads the file from the given location and processes it into the correct
+  file type.
+  Returns `{:ok, image}` if successful or `{:error, error_message}` otherwise
+  """
+  @spec load(uri :: binary) :: {:ok, %{}} | {:error, binary}
   def load(uri) do
     case File.read(uri) do
       {:ok, file} ->
         detect_type_and_process(file)
-      {:error, reason} -> {:error, "Could not open #{uri} due to #{reason}" }
+      {:error, reason} -> {:error, "Could not open #{uri} #{file_error_description(reason)}" }
     end
   end
 
@@ -18,4 +24,7 @@ defmodule Imagineer do
         {:error, "Unknown or unsupported image format."}
     end
   end
+
+  defp file_error_description(:enoent), do: "because the file does not exist."
+  defp file_error_description(reason), do: "due to #{reason}."
 end
--- test/imagineer_test.exs
+++ test/imagineer_test.exs
@@ -3,11 +3,13 @@ defmodule ImagineerTest do
 
   test "loading a non-existent file" do
     uri = "./404.png"
-    {:error, "Could not open #{uri} due to :enoent"} == Imagineer.load(uri)
+    error_message = "Could not open #{uri} because the file does not exist."
+    {:error, ^error_message} = Imagineer.load(uri)
   end
 
   test "loading a file of unknown type" do
-    uri = "./404.png"
-    {:error, "Could not open #{uri} due to :enoent"} == Imagineer.load(uri)
+    uri = "./test/support/images/jpg/black.jpg"
+    error_message = "Unknown or unsupported image format."
+    {:error, ^error_message} = Imagineer.load(uri)
   end
 end
