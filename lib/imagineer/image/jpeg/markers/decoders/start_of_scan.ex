1ac2ea47221824abb5ae4df5e1526226ab0a441f
Chris Maddox <tyre77@gmail.com>
2016-11-06 17:25:00 -0800

    we may have something

--- lib/imagineer/image/jpeg/markers/decoders/start_of_scan.ex
+++ lib/imagineer/image/jpeg/markers/decoders/start_of_scan.ex
@@ -4,55 +4,75 @@ defmodule Imagineer.Image.JPEG.Markers.Decoders.StartOfScan do
   @end_of_image <<255::size(8), 217::size(8)>>
 
   def decode(<<
-      _number_of_color_components::integer-size(8),
+      number_of_components::integer-size(8),
       component_data::binary
     >>,
     image,
     rest_of_content)
   do
-    scan_data = extract_components(component_data, image)
+    scan_data = extract_components(number_of_components, component_data, image)
     |> parse_scan_data(rest_of_content)
-    %{image |
+    new_image = %{image |
         attributes: %{
           scan_data: scan_data
         }
       }
+    {@end_of_image, new_image}
   end
 
-  defp extract_components(component_data, image) do
+  defp extract_components(number_of_components, component_data, image) do
+    Logger.debug("\tParsing #{number_of_components} components")
+    Logger.debug("\tComponent data:")
+    Logger.debug("\t\t#{inspect(component_data)}")
     %{image |
         attributes: %{ image.attributes |
-          components: parse_components(component_data, [])
+          components: parse_components(number_of_components, component_data, [])
         }
       }
   end
 
-  defp parse_components(<<>>, components) do
+  defp parse_components(0, _rest, components) do
     Enum.reverse(components)
   end
 
-  defp parse_components(<<
-                          component_id::integer-size(8),
-                          ac_table::integer-size(4),
-                          dc_table::integer-size(4)
+  defp parse_components(number_of_components,
+    <<
+      component_id::integer-size(8),
+      ac_table::integer-size(4),
+      dc_table::integer-size(4),
+      rest::binary
     >>,
     components)
   do
-    [{component_id, ac_table, dc_table} | components]
+    Logger.debug("\t\t\tcomponent #{length(components) + 1}: #{inspect {component_id, ac_table, dc_table}}")
+    parse_components(
+      number_of_components - 1,
+      rest,
+      [{component_id, ac_table, dc_table} | components]
+    )
+  end
+
+  defp parse_components(number_of_components, another_bin, components) do
+    Logger.debug("UH OH!")
+    Logger.debug("\t#{number_of_components} components left to parse")
+    Logger.debug("\tanother_bin: #{inspect(another_bin)}")
+    Logger.debug("\tcomponents: #{inspect(components)}")
   end
 
   defp parse_scan_data(image, rest_of_content) do
     {scan_data, rest_of_content} = reduce_scan_data(rest_of_content)
   end
 
-  defp reduce_scan_data(@end_of_image), do: <<>>
-  defp reduce_scan_data(<<@end_of_image, _binary>>), do: <<>>
+  defp reduce_scan_data(@end_of_image), do: {<<>>, <<>>}
+  defp reduce_scan_data(<<@end_of_image, binary>>), do: {<<>>, binary}
 
   defp reduce_scan_data(<<255, 255, rest::binary>>) do
-    <<255>> <> reduce_scan_data(rest)
+    {new_data, rest_data} = reduce_scan_data(rest)
+    {<<255>> <> new_data, rest_data}
   end
 
   defp reduce_scan_data(<<a_byte::binary-size(1), rest::binary>>) do
-    a_byte <> reduce_scan_data(rest)
+    {new_data, rest_data} = reduce_scan_data(rest)
+    {a_byte <> new_data, rest_data}
   end
 end
