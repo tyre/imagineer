968ac40798e8a643af27cf42d70f375cea38a392
Chris Maddox <tyre77@gmail.com>
2018-03-25 23:24:54 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Imagineer.Mixfile do
   def project do
     [
       app: :imagineer,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.6",
       deps: deps(),
       source_url: "https://github.com/tyre/imagineer",
